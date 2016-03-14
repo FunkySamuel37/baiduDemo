b39997eb126fcbde28f016a5eb74aa4857038bbb
FunkySamuel37 <masterzhiwei@gmail.com>
2016-03-14 20:57:42 +0800

    finish task 1

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -3,6 +3,11 @@ var sass = require('gulp-sass');
 var less = require('gulp-less');
 var prefix = require('gulp-autoprefixer');
 
+
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+
+
 gulp.task('sass', function() {
   gulp.src('styles/*.scss')
     .pipe(sass())
@@ -22,9 +27,20 @@ gulp.task('copy', function() {
     .pipe(gulp.dest('..'))
 });
 
+gulp.task('imagemin', function() {
+	gulp.src('asset/images/*')
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		}))
+		.pipe(gulp.dest('../asset/images'));
+});
+
 gulp.task('watch', function() {
   gulp.watch(['*.html'], ['copy']);
   gulp.watch(['styles/*.scss', 'styles/*.less'], ['sass', 'less']);
+  gulp.watch(['asset/images/*'], ['imagemin']);
 });
 
-gulp.task('default', ['sass', 'less', 'copy', 'watch']);
+gulp.task('default', ['sass', 'less', 'copy', 'imagemin', 'watch']);
